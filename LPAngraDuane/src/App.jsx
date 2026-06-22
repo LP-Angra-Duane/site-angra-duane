3a969ef2fb70b268c6597c31992ba95f077dfec9
Caio Basilio <basiliocaio2006@gmail.com>
2026-06-22 09:27:02 -0300

    Adicionando o src completo
    terminando de adicionar imagens e componentes, responsável por mim, para inicializar o projeto

---
+++ LPAngraDuane/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Autoridade from './componentes/autoridade/Autoridade';
+// import Faq2 from './componentes/faq/Faq2';
+import Formulario from './componentes/formulario/Formulario';
+// import Whatsapp from './tools/Whatsapp';
+// import HeroFormSection from './componentes/formulario/HeroFormSection';
+
+
+function app02() {
+    return (
+        <>
+            <Autoridade/>
+            <Formulario/>
+        </>
+    );
+}
+
+export default app02;
