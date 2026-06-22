a095f26087212a45cacb6c3bfb2e77e4ddccd543
ODevSocial <motaserious@gmail.com>
2026-06-22 16:39:48 -0300

    adição da parte do Gustavo

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Formulario from './componentes/formulario/Formulario';
 import Oferecimento from './componentes/oferecimento/oferecimento';
 import Beneficios from './componentes/beneficios/Beneficios';
 import Depoimentos from './componentes/depoimentos/Depoimentos';
+import HeroSection from './componentes/hero/HeroSection';
+import FAQ from './componentes/faq/FAQ';
+import Footer from './componentes/footer/Footer';
 // import Whatsapp from './tools/Whatsapp';
 // import HeroFormSection from './componentes/formulario/HeroFormSection';
 
@@ -12,11 +15,14 @@ import Depoimentos from './componentes/depoimentos/Depoimentos';
 function app02() {
     return (
         <>
+            <HeroSection/>
             <Autoridade/>
             <Oferecimento/>
             <Beneficios/>
             <Depoimentos/>
             <Formulario/>
+            <FAQ/>
+            <Footer/>
         </>
     );
 }
